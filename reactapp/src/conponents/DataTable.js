3c2532dd48f0790711b40fb1e1a1b110d74e4ae3
Damian Tapp <dam180@hotmail.com>
2020-07-05 13:40:24 +1000

    added code

--- reactapp/src/conponents/DataTable.js
+++ reactapp/src/conponents/DataTable.js
@@ -14,3 +14,33 @@ function TableData(props) {
           <th>DOB</th>
         </tr>
       </thead>
+      <tbody className="">
+        {props.results.map((result) => (
+          <tr className="table" key={result.login.uuid}>
+            <td>
+              {" "}
+              <img
+                className="
+                        "
+                src={result.picture.medium}
+                alt=""
+              />
+            </td>
+
+            <td>{result.name.first + " " + result.name.last} </td>
+
+            <td>{result.cell}</td>
+            <td className="email">
+              <a href={result.email}>{result.email}</a>
+            </td>
+            <td>
+              <Moment format="MM/DD/YYYY">{result.dob.date}</Moment>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
+
+export default TableData;
